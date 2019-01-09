f61fe26ffe0bcd394a64db54d2126bc50d02f055
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-09 22:11:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_english_meaning(path, emoticon)
   library = load_library(path)
   result = library["get_meaning"][emoticon]
   if result
-    meaning 
+    result
   else
     "Sorry, that emoticon was not found"
   end
